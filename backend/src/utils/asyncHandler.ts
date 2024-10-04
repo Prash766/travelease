31ae57647fc46ad8f4d994760017690f2e95c9a2
Prash766 <prashantshukla766@gmail.com>
2024-10-05 02:49:08 +0530

    feat: Implement asyncHandler and API error utilities

---
+++ backend/src/utils/asyncHandler.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+
+const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+      Promise.resolve(fn(req, res, next)).catch(next);
+    };
+  };
+  
+
+export default asyncHandler
