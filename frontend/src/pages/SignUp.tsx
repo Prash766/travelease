bc83ddcd285d2152bca8e48bd5d302e743e97ef7
Prash766 <prashantshukla766@gmail.com>
2024-10-07 23:00:00 +0530

    Added signup with frontend validation and backend connection for user registration

---
+++ frontend/src/pages/SignUp.tsx
@@ -0,0 +1,242 @@
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { Loader, PlaneTakeoff } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import * as apiClient from "../api-client";
+
+export type SignUpFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+export default function SignUp() {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpFormData>();
+  const mutation = useMutation({
+    mutationFn: apiClient.signUp,
+    onSuccess: () => {
+      console.log("registration success");
+    },
+    onError: (error: Error) => {
+      console.log(error.message);
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
+
+  return (
+    <div
+      className="min-h-screen bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2073&q=80')",
+      }}
+    >
+      <div className="bg-black bg-opacity-50 min-h-screen">
+        <nav className="bg-transparent">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-20">
+              <div className="flex items-center">
+                <Link to="/" className="flex-shrink-0 flex items-center">
+                  <PlaneTakeoff className="h-12 w-12 text-white" />
+                  <span className="text-white text-2xl font-bold ml-2">
+                    TravelVerse
+                  </span>
+                </Link>
+              </div>
+              <div className=" block">
+                <div className="ml-4 flex items-center md:ml-6">
+                  <Link
+                    to="/login"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
+                  >
+                    Login
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="flex items-center justify-center p-4"
+        >
+          <div className="bg-white bg-opacity-90 shadow-md rounded-lg p-8 w-full max-w-md">
+            <div className="text-center mb-8">
+              <h2 className="text-3xl font-bold text-gray-900 mb-2">
+                Create an account
+              </h2>
+              <p className="text-gray-600">
+                Enter your details below to create your TravelVerse account
+              </p>
+            </div>
+            <form onSubmit={onSubmit} className="space-y-6">
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <label
+                    htmlFor="firstName"
+                    className="block text-sm font-medium text-gray-700 mb-1"
+                  >
+                    First Name
+                  </label>
+                  <input
+                    id="firstName"
+                    type="text"
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    {...register("firstName", {
+                      required: "This field is required",
+                    })}
+                    placeholder="John"
+                  />
+                  {errors.firstName && (
+                    <span className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </span>
+                  )}
+                </div>
+                <div>
+                  <label
+                    htmlFor="lastName"
+                    className="block text-sm font-medium text-gray-700 mb-1"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    id="lastName"
+                    type="text"
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    {...register("lastName", {
+                      required: "This field is required",
+                    })}
+                    placeholder="Doe"
+                  />
+                  {errors.lastName && (
+                    <span className="text-sm text-red-500">
+                      {errors.lastName.message}
+                    </span>
+                  )}
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Email
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  {...register("email", {
+                    required: "This field is required",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: "invalid email address",
+                    },
+                  })}
+                  placeholder="john.doe@example.com"
+                />
+                {errors.email && (
+                  <span className="text-sm text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  {...register("password", {
+                    required: "This field is required",
+                    minLength: {
+                      value: 6,
+                      message: "Password must be of at least 6 characters",
+                    },
+                  })}
+                />
+                {errors.password && (
+                  <span className="text-sm text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="confirmPassword"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  id="confirmPassword"
+                  type="password"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  {...register("confirmPassword", {
+                    validate: (val) => {
+                      if (!val) {
+                        return "This field is required";
+                      } else if (watch("password") !== val) {
+                        return "Your Passwords donot Match ";
+                      }
+                    },
+                  })}
+                />
+                {errors.confirmPassword && (
+                  <span className="text-sm text-red-500">
+                    {errors.confirmPassword.message}
+                  </span>
+                )}
+              </div>
+              <button
+                type="submit"
+                disabled={mutation.isPending}
+                className={`w-full py-3 px-4 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
+                  mutation.isPending ? "opacity-50 cursor-not-allowed" : ""
+                }`}
+              >
+                {mutation.isPending && !mutation.error ? (
+                  <div className="flex justify-center items-center space-x-2">
+                    <Loader className="animate-spin" />
+                    <span>Creating account...</span>
+                  </div>
+                ) : (
+                  "Create Account"
+                )}
+              </button>
+            </form>
+            <p className="mt-4 text-center text-sm text-gray-600">
+              Already have an account?{" "}
+              <Link
+                to="/login"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Sign in
+              </Link>
+            </p>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+}
