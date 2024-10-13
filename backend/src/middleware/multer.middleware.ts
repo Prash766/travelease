a84b0f494cbb7766e6625ca2ca8389f6315d31c9
Prash766 <prashantshukla766@gmail.com>
2024-10-13 23:09:44 +0530

    add multer middlware

---
+++ backend/src/middleware/multer.middleware.ts
@@ -0,0 +1,21 @@
+import multer from 'multer'
+
+const storage  = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    },
+
+  })
+  const limits = {
+    fileSize: 5 * 1024 * 1024 // 5 MB
+  };
+  
+  
+  export const upload = multer({
+    storage:storage,
+    limits:limits
+})
+
