6ff611176a2d53487a2d4b104561a937482eecdc
Prash766 <prashantshukla766@gmail.com>
2024-10-13 23:09:17 +0530

    cloudinary config setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,24 +3,30 @@ import cors from 'cors';
 import connectDB from './db/db';
 import cookieParser from 'cookie-parser';
 import errorMiddleware from './middleware/error.middleware';
-import path from 'path'
+import {v2 as cloudinary} from 'cloudinary'
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, '../../frontend/dist')))
 
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET 
+    });
+    
 connectDB()
 
-import UserRouter from './routes/user.routes';
-app.use("/api/v1/users", UserRouter);
 
+import UserRouter from './routes/user.routes';
+import HotelRouter from './routes/hotels.routes'
+app.use("/api/v1/users", UserRouter);
+app.use("/api/v1/hotels", HotelRouter)
 
 app.use(errorMiddleware);
 
