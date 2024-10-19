cfb7095cb736fafc6135023f282b96a3e96a2dac
Prash766 <prashantshukla766@gmail.com>
2024-10-19 16:37:02 +0530

    Enable CORS for all routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,10 @@ const app = express();
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
+  
 }));
+app.options('*', cors())
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
