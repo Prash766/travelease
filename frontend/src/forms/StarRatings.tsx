db173e392c787d0003b98d8368ab4cab83cb5def
Prash766 <prashantshukla766@gmail.com>
2024-10-19 13:19:51 +0530

    refactored

--- frontend/src/forms/StarRatings.tsx
+++ frontend/src/forms/StarRatings.tsx
@@ -1,31 +1,32 @@
 import {motion} from 'framer-motion'
 import { Star } from 'lucide-react'
 import { HotelOtherType } from './HotelDetailsSection'
-type PropType={
+
+type PropType = {
     formData: HotelOtherType
-    handleStar: (star: number)=> void
+    handleStar: (star: number) => void
 }
 
-const StarRatings = ({handleStar , formData}: PropType) => {
+const StarRatings = ({ handleStar, formData }: PropType) => {
   return (
     <div className="space-y-2">
-    <label htmlFor="starRating" className="text-lg font-semibold block">Star Rating</label>
-    <div className="flex justify-between items-center text-4xl">
-      {[1, 2, 3, 4, 5].map((star) => (
-        <motion.button
-          key={star}
-          type="button"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          onClick={()=> handleStar(star)}
-          className={`cursor-pointer ${formData.starRating >= star ? 'text-yellow-400' : 'text-gray-300'}`}
-        >
-          <Star fill={`${formData.starRating >= star ? '#FFFF00' : '#808080'}`} />
-        </motion.button>
-      ))}
+      <label htmlFor="starRating" className="text-lg font-semibold block">Star Rating</label>
+      <div className="flex justify-between items-center text-4xl">
+        {[1, 2, 3, 4, 5].map((star) => (
+          <motion.button
+            key={star}
+            type="button"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            onClick={() => handleStar(star)}
+            className={`cursor-pointer ${formData.starRating >= star ? 'text-yellow-400' : 'text-gray-300'}`}
+          >
+            <Star fill={formData.starRating >= star ? '#FFFF00' : '#808080'} /> {/* Ensure the correct fill color */}
+          </motion.button>
+        ))}
+      </div>
+      {formData.starRating === 0 ? <div className="mt-4 text-red-500 text-sm">Select the Star Rating</div> : null}
     </div>
-      {formData.starRating===0? <div className='mt-4 text-red-500 text-sm'>Select the Star Rating</div>: null}
-  </div>
   )
 }
 
