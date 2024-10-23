1eecad45c02b76ca028458a32561633839bbc501
Prash766 <prashantshukla766@gmail.com>
2024-10-24 01:00:18 +0530

    added search context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AppContextProvider } from './contexts/AppContext.tsx'
 import { Toaster } from 'sonner'
+import { SearchContextProvider } from './contexts/SearchContext.tsx'
 
 const queryClient = new QueryClient({
   defaultOptions:{
@@ -18,8 +19,10 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppContextProvider>
+        <SearchContextProvider>
         <Toaster richColors closeButton position='top-center'/>
     <App />
+        </SearchContextProvider>
       </AppContextProvider>
     </QueryClientProvider>
   </StrictMode>,
